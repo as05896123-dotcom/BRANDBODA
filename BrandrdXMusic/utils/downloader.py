b4bd72d9013e3b2b2bba82da606fe04a9d231374
kindgood612-boop <kindgood612@gmail.com>
2026-01-02 23:39:25 +0200

    Update downloader.py

--- BrandrdXMusic/utils/downloader.py
+++ BrandrdXMusic/utils/downloader.py
@@ -1,34 +1,36 @@
+import os
 from os import path
 import yt_dlp
-from yt_dlp.utils import DownloadError
-
-ytdl = yt_dlp.YoutubeDL(
-    {
-        "outtmpl": "downloads/%(id)s.%(ext)s",
-        "format": "bestaudio[ext=m4a]",
-        "geo_bypass": True,
-        "nocheckcertificate": True,
-    }
-)
-
 
 def download(url: str, my_hook) -> str:
-    ydl_optssx = {
-        "format": "bestaudio[ext=m4a]",
+    # تـحـديـد مـسـار مـلـف الـكـوكـيـز داخـل الـمـجـلـد
+    cookie_path = "cookies/BrandedXMusic.txt"
+    
+    # الـتـحـقـق مـن وجـود الـمـلـف لـضـمـان الـعـمـل
+    if os.path.isfile(cookie_path):
+        active_cookie = cookie_path
+    else:
+        active_cookie = None
+        print(f"تـنـبـيـه: لـم يـتـم الـعـثـور عـلـى الـكـوكـيـز فـي: {cookie_path}")
+
+    ydl_opts = {
+        "format": "bestaudio/best",
         "outtmpl": "downloads/%(id)s.%(ext)s",
         "geo_bypass": True,
         "nocheckcertificate": True,
         "quiet": True,
         "no_warnings": True,
+        "cookiefile": active_cookie,
     }
-    info = ytdl.extract_info(url, False)
+
     try:
-        x = yt_dlp.YoutubeDL(ydl_optssx)
-        x.add_progress_hook(my_hook)
-        dloader = x.download([url])
-    except Exception as y_e:
-        return print(y_e)
-    else:
-        dloader
-    xyz = path.join("downloads", f"{info['id']}.{info['ext']}")
-    return xyz
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.add_progress_hook(my_hook)
+            # بـدء عـمـلـيـة الـتـحـمـيـل واسـتـخـراج الـبـيـانـات
+            info = ydl.extract_info(url, download=True)
+            # إرجـاع الـمـسـار الـحـقـيـقـي لـلـمـلـف الـمـحـمـل
+            return ydl.prepare_filename(info)
+            
+    except Exception as e:
+        print(f"خـطـأ أثـنـاء الـتـحـمـيـل: {e}")
+        return None
