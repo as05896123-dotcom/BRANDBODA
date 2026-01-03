9549c0008854f34408ee7992e3250bb1ee844106
kindgood612-boop <kindgood612@gmail.com>
2026-01-03 09:06:01 +0200

    Update all_tag.py

--- BrandrdXMusic/plugins/admins/all_tag.py
+++ BrandrdXMusic/plugins/admins/all_tag.py
@@ -1,12 +1,12 @@
 import asyncio
 import random
-from pyrogram import Client, filters
+from pyrogram import filters
 from pyrogram.enums import ChatType, ChatMemberStatus
 from pyrogram.errors import UserNotParticipant
 from pyrogram.types import ChatPermissions
+
 from BrandrdXMusic import app
 from BrandrdXMusic.utils.branded_ban import admin_filter
-
 
 SPAM_CHATS = []
 
@@ -21,60 +21,50 @@ SPAM_CHATS = []
             "منشن",
             "الكل",
             "نادي",
-            "للجميع"
+            "للجميع",
         ],
-        prefixes=["/", "@", ".", "#", ""]
+        prefixes=["/", ".", "@", "#", ""],  # مع وبدون /
     )
     & admin_filter
 )
 async def tag_all_users(_, message):
     replied = message.reply_to_message
-    if len(message.command) < 2 and not replied:
-        await message.reply_text(
-            "**» اعــطــنــي نــصــاً لــلــمــنــشــن ، مــثــال »** `تاك السلام عليكم`"
-        )
-        return
-    if replied:
-        SPAM_CHATS.append(message.chat.id)
-        usernum = 0
-        usertxt = ""
-        async for m in app.get_chat_members(message.chat.id):
-            if message.chat.id not in SPAM_CHATS:
-                break
-            usernum += 1
-            usertxt += f"\n⊚ [{m.user.first_name}](tg://user?id={m.user.id})\n"
-            if usernum == 5:
-                await replied.reply_text(usertxt)
-                await asyncio.sleep(2)
-                usernum = 0
-                usertxt = ""
-        try:
-            SPAM_CHATS.remove(message.chat.id)
-        except Exception:
-            pass
-    else:
-        text = message.text.split(None, 1)[1]
 
-        SPAM_CHATS.append(message.chat.id)
-        usernum = 0
-        usertxt = ""
-        async for m in app.get_chat_members(message.chat.id):
-            if message.chat.id not in SPAM_CHATS:
-                break
-            usernum += 1
-            usertxt += f"\n⊚ [{m.user.first_name}](tg://user?id={m.user.id})\n"
-            if usernum == 5:
+    if len(message.command) < 2 and not replied:
+        return await message.reply_text(
+            "اعطني نصاً للمنشن\nمثال: تاك السلام عليكم"
+        )
+
+    SPAM_CHATS.append(message.chat.id)
+
+    usernum = 0
+    usertxt = ""
+
+    async for m in app.get_chat_members(message.chat.id):
+        if message.chat.id not in SPAM_CHATS:
+            break
+
+        if not m.user or m.user.is_bot:
+            continue
+
+        usernum += 1
+        usertxt += f"\n⊚ [{m.user.first_name}](tg://user?id={m.user.id})"
+
+        if usernum == 5:
+            if replied:
+                await replied.reply_text(usertxt)
+            else:
+                text = message.text.split(None, 1)[1]
                 await app.send_message(
                     message.chat.id,
-                    f"{text}\n{usertxt}\n\n|| ➥ لــلإيــقــاف ارســل » بس ||",
+                    f"{text}\n{usertxt}\n\nللإيقاف ارسل: بس",
                 )
-                await asyncio.sleep(2)
-                usernum = 0
-                usertxt = ""
-        try:
-            SPAM_CHATS.remove(message.chat.id)
-        except Exception:
-            pass
+
+            await asyncio.sleep(2)
+            usernum = 0
+            usertxt = ""
+
+    SPAM_CHATS.remove(message.chat.id)
 
 
 @app.on_message(
@@ -96,21 +86,17 @@ async def tag_all_users(_, message):
             "إيقاف",
             "الغاء",
             "إلغاء",
-            "اسكت"
+            "اسكت",
         ],
-        prefixes=["/", "@", "#", ""]
+        prefixes=["/", ".", "@", "#", ""],  # مع وبدون /
     )
     & admin_filter
 )
 async def cancelcmd(_, message):
     chat_id = message.chat.id
-    if chat_id in SPAM_CHATS:
-        try:
-            SPAM_CHATS.remove(chat_id)
-        except Exception:
-            pass
-        return await message.reply_text("**» تــم إيــقــاف الــمــنــشــن بــنــجــاح !**")
 
-    else:
-        await message.reply_text("**» لا يــوجــد مــنــشــن قــائــم حــالــيــاً !**")
-        return
+    if chat_id in SPAM_CHATS:
+        SPAM_CHATS.remove(chat_id)
+        return await message.reply_text("تم إيقاف المنشن بنجاح")
+
+    return await message.reply_text("لا يوجد منشن شغال حالياً")
