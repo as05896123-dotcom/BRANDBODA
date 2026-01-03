c336b3d6fc5152df0efc71383782b34e6cb4400a
kindgood612-boop <kindgood612@gmail.com>
2026-01-03 10:02:13 +0200

    Update couples_db.py

--- BrandrdXMusic/mongo/couples_db.py
+++ BrandrdXMusic/mongo/couples_db.py
@@ -2,35 +2,52 @@ from BrandrdXMusic.utils.mongo import db
 
 coupledb = db.couple
 
-async def _get_lovers(cid: int):
-    lovers = await coupledb.find_one({"chat_id": cid})
-    if lovers:
-        lovers = lovers["couple"]
-    else:
-        lovers = {}
-    return lovers
+
+async def _get_lovers(cid: int) -> dict:
+    """
+    جلب قاموس الأزواج حسب التاريخ
+    """
+    data = await coupledb.find_one({"chat_id": cid})
+    if data and "couple" in data:
+        return data["couple"]
+    return {}
+
 
 async def _get_image(cid: int):
-    lovers = await coupledb.find_one({"chat_id": cid})
-    if lovers:
-        lovers = lovers["img"]
-    else:
-        lovers = {}
-    return lovers
+    """
+    جلب صورة الزوج (إن وجدت)
+    """
+    data = await coupledb.find_one({"chat_id": cid})
+    if data and "img" in data:
+        return data["img"]
+    return None
+
 
 async def get_couple(cid: int, date: str):
+    """
+    جلب زوج يوم معين
+    """
     lovers = await _get_lovers(cid)
     if date in lovers:
         return lovers[date]
-    else:
-        return False
+    return False
 
 
 async def save_couple(cid: int, date: str, couple: dict, img: str):
+    """
+    حفظ زوج اليوم + الصورة
+    """
     lovers = await _get_lovers(cid)
     lovers[date] = couple
+
     await coupledb.update_one(
         {"chat_id": cid},
-        {"$set": {"couple": lovers, "img": img}},
+        {
+            "$set": {
+                "chat_id": cid,
+                "couple": lovers,
+                "img": img,
+            }
+        },
         upsert=True,
-                              )
+    )
