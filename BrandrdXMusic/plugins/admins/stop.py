361527ab4802233c15a8880811791535fe123289
kindgood612-boop <kindgood612@gmail.com>
2026-01-03 09:37:35 +0200

    Update stop.py

--- BrandrdXMusic/plugins/admins/stop.py
+++ BrandrdXMusic/plugins/admins/stop.py
@@ -3,37 +3,39 @@ from pyrogram.types import Message
 
 from BrandrdXMusic import app
 from BrandrdXMusic.core.call import Hotty
-# قمنا بإضافة get_cmode لجلب القناة المرتبطة
-from BrandrdXMusic.utils.database import set_loop, get_cmode
-from BrandrdXMusic.utils.decorators import AdminRightsCheck
+from BrandrdXMusic.core.database import set_loop, get_cmode
+from BrandrdXMusic.core.decorators import AdminRightsCheck
 from BrandrdXMusic.utils.inline import close_markup
 from config import BANNED_USERS
 
 
 @app.on_message(
-    filters.command([
-        "end", "stop", "cend", "cstop",
-        "وقف", "ايقاف", "بس", "اقف"
-    ]) & filters.group & ~BANNED_USERS
+    filters.command(
+        [
+            "end", "stop", "cend", "cstop",
+            "وقف", "ايقاف", "بس", "اقف"
+        ],
+        prefixes=["/", "@", ".", "#", ""]
+    )
+    & filters.group
+    & ~BANNED_USERS
 )
 @AdminRightsCheck
-async def stop_music(cli, message: Message, _, chat_id):
-    # التحقق من النص: هل يحتوي على كلمة "قناة" أو "قناه"؟
-    # أو هل الأمر يبدأ بحرف c بالانجليزي (مثل cstop)
+async def stop_music(client, message: Message, _, chat_id):
     cmd = message.command[0]
-    query = message.text.split(None, 1)[1] if len(message.command) > 1 else ""
-    
-    if cmd.startswith("c") or "قناة" in query or "قناه" in query:
-        try:
-            # جلب معرف القناة المرتبطة
-            chat_id = await get_cmode(chat_id)
-            if not chat_id:
-                return await message.reply_text("خطأ: لا توجد قناة مرتبطة بهذا الجروب.")
-        except:
+
+    # --- دعم القناة المرتبطة ---
+    if cmd.startswith("c"):
+        linked = await get_cmode(chat_id)
+        if not linked:
             return await message.reply_text(_["cplay_4"])
-            
+        chat_id = linked
+
+    # --- إيقاف التشغيل ---
     await Hotty.stop_stream(chat_id)
     await set_loop(chat_id, 0)
+
     await message.reply_text(
-        _["admin_5"].format(message.from_user.mention), reply_markup=close_markup(_)
+        _["admin_5"].format(message.from_user.mention),
+        reply_markup=close_markup(_),
     )
