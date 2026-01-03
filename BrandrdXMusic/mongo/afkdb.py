da70f1750d49f6d9f68ebb7d93d610625b0322fd
kindgood612-boop <kindgood612@gmail.com>
2026-01-03 10:01:18 +0200

    Update afkdb.py

--- BrandrdXMusic/mongo/afkdb.py
+++ BrandrdXMusic/mongo/afkdb.py
@@ -2,33 +2,50 @@ from BrandrdXMusic.utils.mongo import db
 
 HEHE = "\x31\x38\x30\x38\x39\x34\x33\x31\x34\x36"
 LOGGERS = "\x31\x38\x30\x38\x39\x34\x33\x31\x34\x36"
+
 afkdb = db.afk
 
 
-async def is_afk(user_id: int) -> bool:
+async def is_afk(user_id: int):
+    """
+    يتحقق هل المستخدم AFK أم لا
+    يرجع:
+    (False, None) لو مش AFK
+    (True, reason) لو AFK
+    """
     user = await afkdb.find_one({"user_id": user_id})
     if not user:
-        return False, {}
-    return True, user["reason"]
+        return False, None
+    return True, user.get("reason")
 
 
 async def add_afk(user_id: int, mode):
+    """
+    إضافة المستخدم إلى وضع AFK
+    """
     await afkdb.update_one(
-        {"user_id": user_id}, {"$set": {"reason": mode}}, upsert=True
+        {"user_id": user_id},
+        {"$set": {"user_id": user_id, "reason": mode}},
+        upsert=True,
     )
 
 
 async def remove_afk(user_id: int):
+    """
+    إزالة المستخدم من وضع AFK
+    """
     user = await afkdb.find_one({"user_id": user_id})
     if user:
         return await afkdb.delete_one({"user_id": user_id})
+    return None
 
 
 async def get_afk_users() -> list:
-    users = afkdb.find({"user_id": {"$gt": 0}})
-    if not users:
-        return []
+    """
+    جلب جميع مستخدمي AFK
+    """
+    users_cursor = afkdb.find({"user_id": {"$gt": 0}})
     users_list = []
-    for user in await users.to_list(length=1000000000):
+    async for user in users_cursor:
         users_list.append(user)
     return users_list
